9f2a0f0b7701da9f997030a463c51a7009b7d30f
Aleksandra Zupanska <aleksandra.zupanska@student.howest.be>
2025-03-23 02:35:39 +0100

    Physics based jump

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -7,7 +7,6 @@ public class CharacterMover : MonoBehaviour
 {
     [Header("Move")]
     [SerializeField] private float _moveSpeed = 5;
-    [SerializeField] private float _moveDuration = 1;
     [Space]
     [Header("Rotate")]
     [SerializeField] private float _rotateSpeed = 60;
@@ -16,10 +15,16 @@ public class CharacterMover : MonoBehaviour
     [Header("Jump")]
     [SerializeField] LayerMask _groundLayer;
     [SerializeField] private float _groundCheckDistance = 4;
-    [SerializeField] private float _jumpStrength = 1;
-    [SerializeField] private float _jumpHeight = 2;
-    [SerializeField] private float _jumpDuration = 1;
+    [SerializeField] private float _forwardJumpMultiplier = 1.5f;
+    [SerializeField] private float _jumpHeightBoost = 1f;
+    [SerializeField] private Vector2 _minMaxJumpHeight = new Vector2(1,3);
+
+    private float _acceleration; 
+    private Vector3 _previousVelocity;
+    private bool _shouldJump;
+
     private Rigidbody _rigidBody;
+
     void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
@@ -27,20 +32,40 @@ public class CharacterMover : MonoBehaviour
 
     void Update()
     {
+        _acceleration = (_rigidBody.velocity.magnitude - _previousVelocity.magnitude) / Time.deltaTime;
+        _previousVelocity = _rigidBody.velocity;
+
         LookAtMousePos();
 
         if (Input.GetAxis("Vertical") > 0 && IsGrounded())
         {
-            _rigidBody.DOMove(transform.position + (transform.forward * (Input.GetAxis("Vertical") * _moveSpeed)), _moveDuration, false);
+            _rigidBody.velocity = new Vector3(transform.forward.x * _moveSpeed, _rigidBody.velocity.y, transform.forward.z * _moveSpeed);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            _rigidBody.DOJump(transform.position + (transform.forward * _jumpStrength), _jumpHeight, 1, _jumpDuration, false)
-                .SetEase(Ease.InOutSine);
+            _shouldJump = true;
         }
-
     }
+
+    private void FixedUpdate()
+    {
+        if (_shouldJump)
+        {
+            PerformJump();
+            _shouldJump=false;
+        }
+    }
+
+    private void PerformJump()
+    {
+        float jumpHeight = Mathf.Clamp(Mathf.Abs(_acceleration) * _jumpHeightBoost, _minMaxJumpHeight.x, _minMaxJumpHeight.y);
+        Vector3 forwardBoost = transform.forward * _rigidBody.velocity.magnitude * _forwardJumpMultiplier;
+
+        _rigidBody.velocity = new Vector3(_rigidBody.velocity.x, jumpHeight, _rigidBody.velocity.z);
+        _rigidBody.velocity += forwardBoost;
+    }
+
     private void LookAtMousePos()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -61,7 +86,6 @@ public class CharacterMover : MonoBehaviour
     private bool IsGrounded()
     {
         RaycastHit hit; 
-
         if (Physics.Raycast(transform.position, -Vector3.up * _groundCheckDistance, out hit, 1, _groundLayer))
         {
             Debug.DrawRay(transform.position, -Vector3.up * _groundCheckDistance, Color.red, 1);
